9796c59b9dcdab001d86909eb2784ce6ed604fd7
Christopher Pedraza <gabalixreborn@gmail.com>
2024-03-21 15:01:18 -0600

    Encontrar r que satisfaga g^r = mN+1

--- prime-finder.py
+++ prime-finder.py
@@ -1,4 +1,16 @@
 n = 77
 
-for i in range(2, n):
-    pass
+for g in range(2, n):
+    # Check whether g shares a factor with n
+    if n % g == 0:
+        continue
+
+    # Get a g to the power of r that is equals to mN+1, where m is a constant and N is the number we are testing
+    # Test all g's to the power of r until we find one that satisfies the condition of being one more than a multiple of N and
+    # having an even r
+    r = 1
+    while (g**r) % n != 1 or r % 2 == 1:
+        r += 1
+
+    print(f"{g}^{r} = {g**r} (mod {n})")
+    continue
